bd3f895069b8d114b8b9a9624504fa46bb9bafe5
Nafees <abdur17@student.sust.edu>
2022-06-14 05:36:08 +0600

    animation done

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Typewriter } from 'react-typewriting-effect';
 
 const NotFound = () => {
     return (
         <div>
-            <p>Not Found</p>
-        </div>
+        <p className='text-5xl text-center text-primary font-bold my-32'>
+            <Typewriter
+                options={{
+                    strings: ["404 ... Not Found", "Try Again"],
+                    autoStart: true,
+                    loop: true,
+                    deleteSpeed: 30,
+                }}
+            /></p>
+
+    </div>
     );
 };
 
